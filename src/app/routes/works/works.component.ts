1b0543d1e013fe5cd68062de5453b01c695cf93b
Philip <philip.chen@sarbeer.com>
2020-06-16 18:35:19 +0800

    refactor(works.component): refine work display

--- src/app/routes/works/works.component.ts
+++ src/app/routes/works/works.component.ts
@@ -1,16 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { ContentfulService } from '../../core/services/contentful.service';
-import { Entry } from 'contentful';
+import { Component, OnInit } from "@angular/core";
+import { ContentfulService } from "../../core/services/contentful.service";
+import { Entry } from "contentful";
 
 @Component({
-  selector: 'app-works',
-  templateUrl: './works.component.html',
-  styleUrls: ['./works.component.sass'],
+  selector: "app-works",
+  templateUrl: "./works.component.html",
+  styleUrls: ["./works.component.sass"],
 })
 export class WorksComponent implements OnInit {
   cases: Entry<any>[];
+  isAll: boolean;
+  isGraphic = false;
+  isProduct = false;
+  isPhoto = false;
+  isWeb = false;
 
   constructor(private contentfulService: ContentfulService) {}
+
+  checkAll() {
+    return (this.isAll = ![
+      this.isGraphic,
+      this.isPhoto,
+      this.isProduct,
+      this.isWeb,
+    ].includes(true));
+  }
+
+  getCasesByCategoryName(category: string) {
+    this.contentfulService
+      .getCasesByCategoryName(category)
+      .then((res) => (this.cases = res));
+  }
 
   ngOnInit(): void {
     this.contentfulService.getCases().then((result) => {
