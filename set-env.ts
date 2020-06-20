1f24505b8e2f94b65627c353f418379556e7470d
philiptwcn <31626298+philiptwcn@users.noreply.github.com>
2020-06-21 00:59:09 +0800

    2

--- set-env.ts
+++ set-env.ts
@@ -1,35 +1,10 @@
-import { writeFile } from "fs";
-
-// Configure Angular `environment.ts` file path
-const targetPath = "./src/environments/environment.ts";
-
-// Load node modules
-const colors = require("colors");
-require("dotenv").load();
-
-const envConfig = `export const environment = {
-  contentful: {
-    environmentId: '${process.env.environmentId}';
-    spaceId: '${process.env.spaceId}';
-    accessToken: '${process.env.accessToken}';
-  };
-};
-`;
-
-console.log(
-  colors.magenta(
-    "The file `environment.ts` will be written with the following content: \n"
-  )
+const fs = require("fs");
+fs.writeFileSync(
+  "./.env",
+  `contentful:{\n
+    environmentId:${process.env.environmentId},\n
+    spaceId:${process.env.spaceId},\n
+    accessToken=${process.env.accessToken}\n
+}
+  `
 );
-console.log(colors.grey(envConfig));
-writeFile(targetPath, envConfig, function(err) {
-  if (err) {
-    throw console.error(err);
-  } else {
-    console.log(
-      colors.magenta(
-        `Angular environment.ts file generated correctly at ${targetPath} \n`
-      )
-    );
-  }
-});
