cd4dc1260d1335b70484362634e13360775654f3
philiptwcn <31626298+philiptwcn@users.noreply.github.com>
2020-06-20 23:41:33 +0800

    2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment: Env = {
   production: false,
   contentful: {
     environmentId: "ENVIRONMENT_ID",
-    spaceId: "g63ggb2zjua9",
-    accessToken: "MT5CtjQJ6Vcof0qBFqqBs96DxnkjQ4TV3zSXYgesyGo"
+    spaceId: "",
+    accessToken: ""
   }
 };
