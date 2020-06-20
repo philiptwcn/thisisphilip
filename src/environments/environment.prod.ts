6da541e21e556f8e469d45de2bf20810dd80fe5d
philiptwcn <31626298+philiptwcn@users.noreply.github.com>
2020-06-20 23:46:02 +0800

    2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ import { Env } from "./env-model";
 export const environment: Env = {
   production: true,
   contentful: {
-    environmentId: `${process.env.environmentId}`,
-    spaceId: `${process.env.spaceId}`,
-    accessToken: `${process.env.accessToken}`
+    environmentId: "",
+    spaceId: "",
+    accessToken: ""
   }
 };
