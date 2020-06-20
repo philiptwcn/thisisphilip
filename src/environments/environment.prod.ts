42bcd4964c94bc7e76b8e350c5c19af94b00e277
philiptwcn <31626298+philiptwcn@users.noreply.github.com>
2020-06-21 03:26:52 +0800

    test(set-env): test env var

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ import { Env } from "./env-model";
 export const environment: Env = {
   production: true,
   contentful: {
-    environmentId: "",
-    spaceId: "",
-    accessToken: ""
+    environmentId: "{ENVIRONMENT_ID}",
+    spaceId: "{SPACE_ID}",
+    accessToken: "{ACCESS_TOKEN}"
   }
 };
